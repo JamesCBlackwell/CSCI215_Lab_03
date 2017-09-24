4a09178b6afbf4d2ecc4a6b0b0881dca7688c615
JamesCBlackwell <31408542+JamesCBlackwell@users.noreply.github.com>
2017-09-24 18:38:45 -0400

    Add files via upload

---
+++ bsa_calc.js
@@ -0,0 +1,79 @@
+function calculate() {
+
+    var female = document.getElementsByName("sex")[1].checked;
+    var male = document.getElementsByName("sex")[0].checked;
+
+    var weight = document.getElementsByName("weight")[0].value;
+    var height = document.getElementsByName("height")[0].value;
+
+    console.log( 'Male = ' + male );
+    console.log( 'Weight = ' + weight );
+    console.log( 'Height = ' + height );
+
+    // -------------------------------
+    // Conversions
+    // -------------------------------
+    // lbs to kg: 1 lb = 0.454 kg
+    // inch to meter: 1 in = 0.0254 m
+    // inch to cm: 1 in = 2.54 cm
+
+    // ==========================================
+    // Todo: Perform conversion calculations here
+    var weightkg = (weight * 0.454)
+    var heightm = (height * 0.0254)
+    var heightcm = (height * 2.54)
+    // -------------------------------
+    // Body Surface Area (BSA)
+    // -------------------------------
+
+    var BSA = 0;
+
+    // ==========================================
+    // Todo: Perform BSA calculation here
+var BSA1 = (heightcm * weightkg) ;
+var BSA2 = (BSA1 * 3600) ;
+    BSA = Math.sqrt(BSA2) ;
+    // -------------------------------
+    // Ideal Body Weight (IBW)
+    // -------------------------------
+
+    var IBW = 0;
+
+    if ( female ) {
+
+        // ==========================================
+        // Todo: Perform female IBW calculation here
+var IBW = ((height - 60) * 2.3 + 45.5) ;
+
+    } else if ( male ) {
+
+        // ==========================================
+        // Todo: Perform male IBW calculation here
+var IBW = ((height - 60) * 2.3 + 50) ;
+
+    }
+
+    // -------------------------------
+    // Body Mass Index (BMI)
+    // -------------------------------
+
+    var BMI = 0;
+
+    // ==========================================
+    // Todo: Perform BMI calculation here
+
+    var BMI = weightkg / Math.pow(heightm,2) ;
+    // Do not modify this function call
+    display( BSA, IBW, BMI );
+
+}
+
+// ------------------------------------
+// Do not modify this function
+function display( BSA, IBW, BMI ) {
+
+    var message = 'Body Surface Area = ' + BSA.toPrecision(4) + ' m^2\nIdeal Body Weight = ' + IBW.toPrecision(4) + ' kg\nBody Mass Index = ' + BMI.toPrecision(4) + ' kg/m^2';
+
+    document.getElementById("output").innerHTML=message;
+
+}
